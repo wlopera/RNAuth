a8c7076634dcd3c264ec621c609e4853a0687f49
William Lopera <lopera.william@gmail.com>
2023-10-02 10:39:32 -0500

    Llamadas a Api de Backend y uso del token de autenticacion

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchMessage, setFetchMessage] = useState("");
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://react-native-expenses-14061-default-rtdb.firebaseio.com/message.json?auth=${token}`
+      )
+      .then((response) => {
+        console.log("respuesta:", response.data);
+        setFetchMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Bienvenido!</Text>
       <Text>Autenticado exitosamente!</Text>
+      <Text>{fetchMessage}</Text>
     </View>
   );
 }
@@ -14,13 +35,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
